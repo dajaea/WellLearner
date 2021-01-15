2730888be9e2d0f0528fce5b9c7af7e458e11533
dajaea <dajaea13@gmail.com>
2021-01-15 12:30:35 -0600

    basic app files cleaned

--- midcourse_app/global.R
+++ midcourse_app/global.R
@@ -1,11 +1,27 @@
+
 library(shiny)
 library(tidyverse)
 library(shinydashboard)
 
-health_data <- read_csv("../data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
+health_data <- read_csv("data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
 
 health_edu <- health_data %>%
-  drop_na() %>%
-  filter(Group == "By Education") %>%
-  mutate(Value = as.numeric(as.character(Value))) %>%
-  mutate(`Time Period` = as.numeric(as.character(`Time Period`)))
+  drop_na() 
+  #filter(Group == "By Education") %>%
+  #mutate(Value = as.numeric(as.character(Value))) %>%
+  #mutate(`Time Period` = as.numeric(as.character(`Time Period`)))
+
+
+#****************
+
+# library(shiny)
+# library(tidyverse)
+# library(shinydashboard)
+# 
+# health_data <- read_csv("data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
+# 
+# health_edu <- health_data %>%
+#   drop_na() 
+#   filter(Group == "By Education") %>%
+#   #mutate(Value = as.numeric(as.character(Value))) %>%
+#   #mutate(`Time Period` = as.numeric(as.character(`Time Period`)))
--- midcourse_app/ui.R
+++ midcourse_app/ui.R
@@ -1,35 +1,63 @@
-library(shiny)
-
 shinyUI(
+  
+  dashboardPage(
     
-    dashboardPage(
-        
-        # Application title
-        dashboardHeader(title ="WellLearner Explorer"),
-        
-        # Sidebar with a slider input for number of bins 
-        
-        dashboardSidebar(
-            selectInput("Illness",
-                        "Choose an Illness:",
-                        choices = Indicator,
-                        selected = Indicator[1])
-            #fileInput("file",
-            #          "Choose a file:"
-            
-        )
-        
+    # Application title
+    dashboardHeader(title ="WellLearner Explorer"),
+    
+    # Sidebar with a slider input for number of bins 
+    
+    dashboardSidebar(
+      selectInput("Illness",
+                  "Choose an Illness:",
+                  choices = health_edu$Indicator,
+                  selected = health_edu$Indicator[1])
+      
     ),
     
+  
     # Show a plot of the generated distribution
     dashboardBody(
-        fluidRow(
-            column(width=8,
-                   box(width=20,
-                       plotOutput("linegraph")
-                   )
-            )
+      plotOutput("linegraph")
+               
         )
-    )
-)
-)
+
+      ))
+
+# library(shiny)
+# 
+# shinyUI(
+#     
+#     dashboardPage(
+#         
+#         # Application title
+#         dashboardHeader(title ="WellLearner Explorer"),
+#         
+#         # Sidebar with a slider input for number of bins 
+#         
+#         dashboardSidebar(
+#             selectInput("Illness",
+#                         "Choose an Illness:",
+#                         choices = Indicator,
+#                         selected = Indicator[1])
+#             #fileInput("file",
+#             #          "Choose a file:"
+#             
+#         )
+#         
+#     ),
+#     
+#     # Show a plot of the generated distribution
+#     dashboardBody(
+#         fluidRow(
+#             column(width=8,
+#                    box(width=20,
+#                        plotOutput("linegraph")
+#                    )
+#             )
+#         )
+#     )
+# )
+# )
+
+

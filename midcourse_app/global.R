d99a592222fa868affb42f958d8ba1de2771f3b0
dajaea <dajaea13@gmail.com>
2021-01-15 15:23:15 -0600

    app by education [MVP]

--- midcourse_app/global.R
+++ midcourse_app/global.R
@@ -6,8 +6,9 @@ library(shinydashboard)
 health_data <- read_csv("data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
 
 health_edu <- health_data %>%
-  drop_na() 
-  #filter(Group == "By Education") %>%
+  select(-c(`Quartile range`)) %>%
+  filter(Group == "By Education") %>%
+  drop_na()
   #mutate(Value = as.numeric(as.character(Value))) %>%
   #mutate(`Time Period` = as.numeric(as.character(`Time Period`)))
 
--- midcourse_app/server.R
+++ midcourse_app/server.R
@@ -1,12 +1,25 @@
 shinyServer(function(input, output) {
   
   output$linegraph <- renderPlot({
+    #health_edu %>%
+    #  filter(Indicator == input$Illness) %>%
+    #  ggplot(aes(x = `Time Period`, y = Value)) +
+    #  geom_line(aes(group = Subgroup, color = Subgroup))
+    ###I deleted most, please add back
+    
     health_edu %>%
       filter(Indicator == input$Illness) %>%
+      #summarise(health_edu$ind_mean = mean('Indicator')) %>%
       ggplot(aes(x = `Time Period`, y = Value)) +
-      geom_line(aes(group = Subgroup, color = Subgroup))
-    ###I deleted most, please add back
-
+      geom_line(aes(group = Subgroup, color = Subgroup)) +
+      ggtitle('TITLE TEST') +
+      xlab('x lab test') +
+      ylab('y lab test') +
+      #geom_ribbon(aes(ymin=`Low CI`, ymax = `High CI`, group = Subgroup, fill = Subgroup, alpha = .1)) +
+      #scale_y_continuous(labels = scales::comma) +
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+      theme(plot.title = element_text(hjust = 0.5))
+      
   })
 })
 

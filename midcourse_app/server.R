128bcbcf54c9064fe67b261f61bf4638b49a4c2f
dajaea <dajaea13@gmail.com>
2021-01-21 13:29:35 -0600

    formatting Ed. Attainment plot

--- midcourse_app/server.R
+++ midcourse_app/server.R
@@ -4,16 +4,25 @@ shinyServer(function(input, output) {
   output$ed_graph <- renderPlot({
     health_edu %>%
       filter(Indicator == input$Illness) %>%
-      ggplot(aes(x = `Time Period`, y = Value)) +
-      #aes(x = reorder(`Bachelor's degree or higher`, `Some college/Associate's degree`, `High school diploma or GED`, `Less than a high school diploma`))    +
+      ggplot(aes(x = `Time Period Label`, y = Value)) +
+      # <trying to show weeks from Time Period Label column, 
+      # I got it by using Time Period Label on line 7 but now ordered alphabetically>
+      #scale_x_date(breaks = `Time Period Label`) +
+      #scale_x_discrete(breaks = c(`Time Period Label`)) +
       geom_line(aes(group = Subgroup, color = Subgroup)) +
       ggtitle('U.S. Mental Illness Over 7.5 Months in 2020 (Education)') +
       xlab('Survey Week') +
       ylab('Percent of U.S.') +
+      # <trying to make error bands>
       #geom_ribbon(aes(ymin=`Low CI`, ymax = `High CI`, group = Subgroup, fill = Subgroup, alpha = .1)) +
-      #scale_x_date(breaks = `Time Period Label`)
-      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
-      theme(plot.title = element_text(hjust = 0.5))
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1, size = 13)) +
+      theme(axis.text.y = element_text(size = 13)) +
+      theme(plot.title = element_text(hjust = 0.5, size = 13, face = "bold")) +
+      theme(legend.text = element_text(size = 13)) +
+      theme(legend.key.width = unit(3,"cm"))
+      # <trying to re-order legend> 
+      #theme(legend.text = reorder("Bachelor's degree or higher", "Some college/Associate's degree", "High school diploma or GED", "Less than a high school diploma"))
+      #legend(limits = c("Bachelor's degree or higher", "Some college/Associate's degree", "High school diploma or GED", "Less than a high school diploma"))
     
   })
   
@@ -48,6 +57,16 @@ shinyServer(function(input, output) {
       theme(plot.title = element_text(hjust = 0.5))
     
   })
+  
+  #output$summary_table <- reactive({
+  #  req(input$column)
+  #  health_edu %>%
+  #    group_by(!!sym(input$column)) %>%
+  #    summarise (value = n(), yield = round(mean(value)*100, 1)) %>%
+  #    mutate(Pct_of_value = paste0(round(100 * value/sum(value), 0), "%"))
+  #})
+  
+  #output$table <- renderTable(summary_table())
   
 })
 
--- midcourse_app/ui.R
+++ midcourse_app/ui.R
@@ -2,7 +2,7 @@
 shinyUI(
   
   navbarPage("WellLearner Explorer",
-
+             
              # Output plots  ...#do a sidebar layout for each
              tabPanel("Ed. Attainment",
                       sidebarLayout(
@@ -11,70 +11,78 @@ shinyUI(
                                       choices = health_edu$Indicator,
                                       selected = health_edu$Indicator[1])),
                         mainPanel(plotOutput("ed_graph"))
-                     )),
-             
-             tabPanel("By Age",
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput("Illness","Choose an Illness:",
-                                      choices = health_age$Indicator,
-                                      selected = health_age$Indicator[1])),
-                        mainPanel(plotOutput("age_graph"))
-                      )),     
-             
-             tabPanel("Education + Age",
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput("Illness","Choose an Illness:",
-                                      choices = diploma_bachelors_29$Indicator,
-                                      selected = diploma_bachelors_29$Indicator[1])),
-                        mainPanel(plotOutput("ed_age_graph"))
-                      ))
-             
-             
-             #mainPanel(
-             #  tabsetPanel(
-             #    tabPanel("Plot", plotOutput("plot")), 
-             #    tabPanel("Summary", verbatimTextOutput("summary")), 
-             #    tabPanel("Table", tableOutput("table"))   
-  )
-)
-
-
-# library(shiny)
-# 
-# shinyUI(
-#     
-#     dashboardPage(
-#         
-#         # Application title
-#         dashboardHeader(title ="WellLearner Explorer"),
-#         
-#         # Sidebar with a slider input for number of bins 
-#         
-#         dashboardSidebar(
-#             selectInput("Illness",
-#                         "Choose an Illness:",
-#                         choices = Indicator,
-#                         selected = Indicator[1])
-#             #fileInput("file",
-#             #          "Choose a file:"
-#             
-#         )
-#         
-#     ),
-#     
-#     # Show a plot of the generated distribution
-#     dashboardBody(
-#         fluidRow(
-#             column(width=8,
-#                    box(width=20,
-#                        plotOutput("linegraph")
-#                    )
-#             )
-#         )
-#     )
-# )
-# )
-
-
+                        #sidebarPanel(
+                        #  selectInput(inputId ="column",
+                        #              label = "Choose Column for Summary",
+                        #              choices = names(health_edu),
+                        #              selected = "grouping_letter"),
+                        #  tableOutput('table')
+                        
+                      )),
+                      
+                      tabPanel("By Age",
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   selectInput("Illness","Choose an Illness:",
+                                               choices = health_age$Indicator,
+                                               selected = health_age$Indicator[1])),
+                                 mainPanel(plotOutput("age_graph"))
+                               )),     
+                      
+                      tabPanel("Education + Age",
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   selectInput("Illness","Choose an Illness:",
+                                               choices = diploma_bachelors_29$Indicator,
+                                               selected = diploma_bachelors_29$Indicator[1])),
+                                 mainPanel(plotOutput("ed_age_graph"))
+                               ))
+                      
+                      #mainPanel(
+                      #  tabsetPanel(
+                      #    tabPanel("Plot", plotOutput("plot")), 
+                      #    tabPanel("Summary", verbatimTextOutput("summary")), 
+                      #    tabPanel("Table", tableOutput("table"))   
+             )
+        )     
+  
+  
+  
+  # library(shiny)
+  # 
+  # shinyUI(
+  #     
+  #     dashboardPage(
+  #         
+  #         # Application title
+  #         dashboardHeader(title ="WellLearner Explorer"),
+  #         
+  #         # Sidebar with a slider input for number of bins 
+  #         
+  #         dashboardSidebar(
+  #             selectInput("Illness",
+  #                         "Choose an Illness:",
+  #                         choices = Indicator,
+  #                         selected = Indicator[1])
+  #             #fileInput("file",
+  #             #          "Choose a file:"
+  #             
+  #         )
+  #         
+  #     ),
+  #     
+  #     # Show a plot of the generated distribution
+  #     dashboardBody(
+  #         fluidRow(
+  #             column(width=8,
+  #                    box(width=20,
+  #                        plotOutput("linegraph")
+  #                    )
+  #             )
+  #         )
+  #     )
+  # )
+  # )
+  
+  
+  
